db796c4b4aa03f47044850a35ea35849af2d2f19
Isaias Medina <isamed92@gmail.com>
2022-08-09 13:20:06 -0300

    add login background design

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('LoginScreen'),
-      ),
-    );
+    return const Scaffold(body: AuthBackground());
   }
 }
