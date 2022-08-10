63e59bdb13481b572ef93ffdf5a5b2f00b955714
Isaias Medina <isamed92@gmail.com>
2022-08-09 23:40:43 -0300

    add login style form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -22,13 +22,51 @@ class LoginScreen extends StatelessWidget {
               'Login',
               style: Theme.of(context).textTheme.headline4,
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
-            Text('Formulario')
+            _LoginForm()
           ]),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Text(
+          'Crear una nueva cuenta',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         )
       ]),
     )));
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+          child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
+                hintText: 'john.doe@email.com',
+                labelStyle: TextStyle(color: Colors.grey),
+                prefixIcon: Icon(
+                  Icons.alternate_email_sharp,
+                  color: Colors.deepPurple,
+                ),
+                labelText: 'Correo'),
+          )
+        ],
+      )),
+    );
   }
 }
